c6b902c46568c5f8f44923863e123e62f6e10abe
s-waite <samwaite11@gmail.com>
2022-10-06 16:01:23 -0600

    refactor: Remove old comments and set as private

--- lib/src/utility/epub.dart
+++ lib/src/utility/epub.dart
@@ -1,4 +1,3 @@
-// TODO: use path seperator Platform.pathSeperator instead of hardcoding forward slash
 import 'dart:io';
 import 'dart:developer' as developer;
 
@@ -10,17 +9,17 @@ import 'package:loro/src/entity/book.dart';
 import 'package:loro/src/database/database.dart';
 import 'package:archive/archive_io.dart';
 
-//TODO: create new posix filename and artist folder, copy file to folder with new name,
 //ensure book hasnt already been added
 // Add book info to database
 // database column for relative path
 // books folder path saved somewhere
-// TODO: could store list of books in global riverpod that everything listens to
 String logSource = 'epub.dart';
 String ps = Platform.pathSeparator;
 
 /// As an inherited widget, any widget that is a child of Epub can acess the bookNotifier;
 class Epub {
+  Epub._();
+
   static Future<Map<String, String>> getMetadata(File epub) async {
     Map<String, String> metaData = {};
     var bytes = await epub.readAsBytes();
@@ -77,55 +76,6 @@ class Epub {
     return ("${dir.path}${ps}cover.$imageFiletype");
   }
 
-//
-//
-//   static void loadEpub(
-//
-//   // Get metadata
-//   // Use metadata to create book dir
-//   // Copy book to dir
-//       File epub, ValueNotifier<List<Book>> bookNotifier) async {
-//     var bytes = await epub.readAsBytes();
-//     EpubBook epubBook = await EpubReader.readBook(bytes);
-//     String title = epubBook.Title.toString();
-//     String author = epubBook.Author.toString();
-//
-//     // Copy book to correct folder after it has been created
-//
-//     LocalFileSystem fs = const LocalFileSystem();
-//     createBookDir(author, title, fs);
-//     // Copy the epub to the loro library
-//     // String path = "${dir.path}/$author - $title.epub";
-//     //   epub.copy(path);
-//     //   final database =
-//     //       await $FloorAppDatabase.databaseBuilder('app_database.db').build();
-//     //   BookDAO bookDAO = database.bookDao;
-//     //
-//     //   bookDAO.insertBook(Book(title: title, authorName: author, path: path));
-//     //   bookNotifier.value = await bookDAO.getAllBooks();
-//     //
-//     //   final inputFileStream = InputFileStream(epub.path);
-//     //   final archive = ZipDecoder().decodeBuffer(inputFileStream);
-//     //   final String firstImageName = epubBook.Content!.Images!.values.first.FileName!;
-//     //   var firstImage = archive.findFile(firstImageName);
-//     //   firstImage!.writeContent(OutputFileStream("${dir.path}/$firstImageName"));
-//     //
-//     //
-//     // String userHome = Platform.environment['HOME'] ??
-//     //     Platform.environment['USERPROFILE'] as String;
-//     // String libraryPath = '$userHome/Loro Library';
-//     // String bookPath = '$libraryPath/$author/$title';
-//     // //
-//     // fs.directory(bookPath).create(recursive: true);
-//     // });
-//   }
-//
-//   /// Returns a string formatted for filesystems
-//   static String stringToFileName(String input) {
-//     return input.replaceAll(RegExp(' '), "_");
-//   }
-//
-  // Creates and returns the directory to store the book
   static Future<Directory> createBookDir(
       String author, String title, String libraryName) async {
     String userHome = Platform.environment['HOME'] ??
