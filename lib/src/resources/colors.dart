425274b76105fbed1b3f9a75b0a4013e855e8d88
s-waite <samwaite11@gmail.com>
2022-10-06 16:00:25 -0600

    feat: add colors to resources

---
+++ lib/src/resources/colors.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+const primary = Color(0xff1D7CAF);
+
+const danger = Color(0xffff7476);
+const dangerAccent = Color(0Xffffdddd);
+
+const success = Color(0xff5cc163);
+const successAccent = Color(0xffd9f0d8);
+
+const warning = Color(0xffff8a5b);
+const warningAccent = Color(0xffffe2d6);
+
+const complete = Color(0xff6e4553);
+const completeAccent = Color(0xffdbd1d4);
+
+const info = Color(0xff166088);
+const infoAccent = Color(0xffc8d7e1);
